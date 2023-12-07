b937412d87e844bcd2338181ebc8d01ff12d9a5c
Olli Salminen <ollisalminen@edu7-80.turkuamk.fi>
2023-12-07 12:03:28 +0200

    kurssit order muutettu

--- TimeManagementTool/TimeManagementToolApp/views.py
+++ TimeManagementTool/TimeManagementToolApp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Kurssi
+from django.db.models.functions import Lower
 
 # Create your views here.
 
 def index(request):
     #etusivu ajanhallinnalle
-    kurssit = Kurssi.objects.all()
-    return render(request, 'TimeManagementToolApp/index.html',{'kurssit': kurssit})
+   kurssit = Kurssi.objects.annotate(lower_kurssi=Lower('kurssi')).order_by('lower_kurssi')
+   return render(request, 'TimeManagementToolApp/index.html',{'kurssit': kurssit})
