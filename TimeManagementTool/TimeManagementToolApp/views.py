99ed565c82505b24430002f34ab5ca813864cc31
opa <olli.k.salminen@gmail.com>
2023-12-07 13:13:27 +0200

    sorting updated

--- TimeManagementTool/TimeManagementToolApp/views.py
+++ TimeManagementTool/TimeManagementToolApp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Kurssi
+from django.db.models.functions import Lower
+
 
 # Create your views here.
 
 def index(request):
     #etusivu ajanhallinnalle
-    kurssit = Kurssi.objects.all()
+    kurssit = Kurssi.objects.annotate(lower_kurssi=Lower('kurssi')).order_by('lower_kurssi')
     return render(request, 'TimeManagementToolApp/index.html',{'kurssit': kurssit})
 
 def kurssit(request):
