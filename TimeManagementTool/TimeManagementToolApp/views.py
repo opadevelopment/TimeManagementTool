4a59ec80566f21ca026a92da26cc4ce4a1c20aa6
Päivi Leskinen <Paivi.Leskinen@edu.turkuamk.fi>
2023-12-07 15:04:49 +0200

    Lisätty deadline info etusivulle

--- TimeManagementTool/TimeManagementToolApp/views.py
+++ TimeManagementTool/TimeManagementToolApp/views.py
@@ -11,13 +11,13 @@ def index(request):
     return render(request, 'TimeManagementToolApp/index.html',{'kurssit': kurssit})
 
 def kurssit(request):
-    #näytä kaikki kurssit
+    #kaikki kurssit
     kurssit = Kurssi.objects.order_by('id')
     context = {'kurssit': kurssit}
     return render(request, 'TimeManagementToolApp/kurssit.html', context)
 
 def kurssi(request, kurssi_id):
-    #näytä yhden kurssin lisätyt tehtävät
+    #yhden kurssin lisätyt tehtävät
     kurssi = Kurssi.objects.get(id=kurssi_id)
     tehtavat = kurssi.teht_set.order_by('teht')
     context = {'kurssi': kurssi, 'teht': tehtavat}
