6d02445258dc0f3da921463ee6228810212ec31e
Päivi Leskinen <Paivi.Leskinen@edu.turkuamk.fi>
2023-12-07 09:41:37 +0200

    admin sivu tehty

--- TimeManagementTool/TimeManagementTool/settings.py
+++ TimeManagementTool/TimeManagementTool/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #omat apit
+    'TimeManagementToolApp',
+
+    #defaultit
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
