1a7becf1ff07871db20a25b880e0bbe3cacf70e6
niklas.rasanen <niklas.rasanen@edu.turkuamk.fi>
2023-12-12 18:40:00 +0200

    Login

--- TimeManagementTool/TimeManagementTool/settings.py
+++ TimeManagementTool/TimeManagementTool/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #omat apit
     'TimeManagementToolApp',
+    'accounts',
 
     #defaultit
     'django.contrib.admin',
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "TimeManagementToolApp:index"
+LOGOUT_REDIRECT_URL = 'accounts:login'
--- TimeManagementTool/accounts/urls.py
+++ TimeManagementTool/accounts/urls.py
@@ -1,8 +1,9 @@
-#url patternit käyttäjille
+"""Defines URL patterns for accounts."""
 
 from django.urls import path, include
 
 app_name = 'accounts'
 urlpatterns = [
+    # Include default auth urls.
     path('', include('django.contrib.auth.urls')),
 ]
